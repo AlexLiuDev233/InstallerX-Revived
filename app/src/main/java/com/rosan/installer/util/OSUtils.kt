54de6c8b81707a834f07077893a41fc6d110077a
AlexLiuDev233 <wzylin11@outlook.com>
2025-11-26 22:43:32 +0800

    perf: migrate system property get to stub file

--- app/src/main/java/com/rosan/installer/util/OSUtils.kt
+++ app/src/main/java/com/rosan/installer/util/OSUtils.kt
@@ -1,6 +1,7 @@
 package com.rosan.installer.util
 
 import android.annotation.SuppressLint
+import android.os.SystemProperties
 import com.rosan.installer.data.reflect.repo.ReflectRepo
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.get
@@ -33,19 +34,6 @@ object OSUtils : KoinComponent {
      */
     @SuppressLint("PrivateApi")
     private fun getSystemProperty(key: String): String? {
-        try {
-            val clz = Class.forName("android.os.SystemProperties")
-            // Try public get(key, def) first
-            val method = reflect.getMethod(clz, "get", String::class.java, String::class.java)
-                ?: reflect.getDeclaredMethod(clz, "get", String::class.java, String::class.java)
-                ?: return null
-
-            // It's a static method: pass null as the instance. Use empty string as default.
-            val value = method.invoke(null, key, "") as? String
-            return value?.takeIf { it.isNotEmpty() }
-        } catch (t: Throwable) {
-            t.printStackTrace()
-        }
-        return null
+        return SystemProperties.get(key)
     }
 }
